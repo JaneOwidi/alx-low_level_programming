2cfb75b7ee7a487302d5d951a8c47d60fd68e527
JaneOwidi <janeowidi@gmail.com>
2023-02-21 06:35:06 -0800

    Hello printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - produce an output according to a format
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
