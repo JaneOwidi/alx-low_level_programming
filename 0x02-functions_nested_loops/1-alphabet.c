71dcc0f5b613fef433aaeb57e0c712b1053bd04a
JaneOwidi <janeowidi@gmail.com>
2023-02-22 10:42:31 -0800

    print alphabet x10

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- * main - print alphabet in lowercase
+ * print_alphabet - print alphabet in lowercase
  *
- * Return: always 0
+ * Return: always 0;
  */
 void print_alphabet(void)
 {
