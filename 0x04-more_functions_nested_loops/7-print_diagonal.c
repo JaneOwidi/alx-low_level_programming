1a0e444ddde134feeaa6d6c1f42fd349a774076c
JaneOwidi <janeowidi@gmail.com>
2023-02-24 11:35:52 -0800

    prints diagonals

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_diagonal - a function that drws a diagonal line
+ * @n: input number of times
+ * Return a diagonal
+ */
+void print_diagonal(int n)
+{
+	int co, sp;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (co = 1; co <= n; co++)
+		{
+			for (sp = 1; sp < co; sp++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+}
