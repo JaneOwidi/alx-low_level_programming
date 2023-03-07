b63a12fd3f14ca62989ca42b274d10c0969285d3
JaneOwidi <janeowidi@gmail.com>
2023-03-07 05:55:13 -0800

    prints chessboard

---
+++ 0x07-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * _putchar - -writes the character c to stdout
+ * @c: function parameter
+ *
+ * Return: 1 on success and -1 on error and error is set appropriately
+ */
+int _putchar(char c)
+{
+        return (write(1, &c, 1));
+}
